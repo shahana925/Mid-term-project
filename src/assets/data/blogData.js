d498fdab3a40399cc5fd9d9f56e848a38000b5ab
shahana925 <shahana.toufiq@gmail.com>
2024-08-08 02:56:41 +0500

    first commit

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,49 @@
+// import images from all-images/blog-img directory
+import img01 from "../all-images/blog-img/blog-1.jpg";
+import img02 from "../all-images/blog-img/blog-2.jpg";
+import img03 from "../all-images/blog-img/blog-3.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive cars",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img01,
+    description:
+
+"When traveling or even for special events, selecting a reliable car service provider can be of the essence. Transportation is such an important part of the overall experience. Whether getting transportation to or from the airport or ordering a luxurious car/limousine for a concert or sports event, [Company Name] is ready to serve you",
+     quote:
+"Save countless hours each month crafting business proposals.",
+  },
+
+  {
+    id: 2,
+    title: "If your car battery is down",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img02,
+    description:
+      " When traveling or even for special events, selecting a reliable car service provider can be of the essence. Transportation is such an important part of the overall experience. Whether getting transportation to or from the airport or ordering a luxurious car/limousine for a concert or sports event, our company is ready to serve you",
+    quote:
+      "Save countless hours each month crafting business proposals",
+  },
+
+  {
+    id: 3,
+    title: "The best way to give trip",
+    author: "Muhib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img03,
+    description:
+
+"When traveling or even for special events, selecting a reliable car service provider can be of the essence. Transportation is such an important part of the overall experience. Whether getting transportation to or from the airport or ordering a luxurious car/limousine for a concert or sports event, [Company Name] is ready to serve you",
+    quote:
+   "Save countless hours each month crafting business proposals",
+  },
+];
+
+export default blogData;
